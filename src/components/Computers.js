59b2f5bd1c5c406479dd4a372c3bd29fa9593731
Rachkov1 <snoopi.rox@gmail.com>
2018-08-20 19:35:59 +0300

    pagination

--- src/components/Computers.js
+++ src/components/Computers.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
+import {Button} from 'react-bootstrap';
 import {Computer} from "./Computer";
 
 class Computers extends Component {
+    state = {
+        count: 10
+    };
+
+    handleClick = (e) => {
+        e.preventDefault();
+        this.state.count += 10;
+        this.setState({count: this.state.count})
+    };
+
     renderTemp () {
         const { data } = this.props;
-        let temp = null;
 
-        if(data.length) {
-            temp = data.map(function (item, index) {
-                return <Computer key={index} data={item}/>
-            })
-        }
+        const temp = data.map(function (item, index) {
+            return <Computer key={index} data={item}/>
+        });
 
-        return temp
+        return temp.slice(0, this.state.count)
     }
 
     render () {
         return (
             <div>
                 {this.renderTemp()}
+                <div className="showMore">
+                    <Button onClick={this.handleClick}>Показать ещё</Button>
+                </div>
             </div>
         )
     }
